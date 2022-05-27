01f35aef91b01b76cfed67362642b8d3e29b8ffe
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-05-27 15:08:01 -0700

    button hover styled

--- src/components/CreativeSolution.js
+++ src/components/CreativeSolution.js
@@ -5,12 +5,19 @@ import Styles from "../assets/style/components_style/CreativeSolution.module.scs
 const CreativeSolution = () => {
   return (
     <>
-      <div className="container-fluid">
-        <div className="row">
-          <div className={Styles.CreativeContainer}>
-            <h2>Creative Solutions by Professional Designers</h2>
-            <p>Cras vel cursus libero. Vestibulum porttitor nunc enim, quis congue leo fringilla ut. Quisque in lacus lacus. Nunc malesuada nisi at porta convallis. In posuere interdum erat, et bibendum arcu. Etiam quis viverra diam.</p>
-            <button>Read More</button>
+      <div className={`${Styles.creativeContainer} container-fluid p-0`}>
+        <div className="row m-0 g-4">
+          <div className="col-md-6">
+            <div className={Styles.left}>
+              <h2>Creative Solutions by Professional Designers</h2>
+              <p>Cras vel cursus libero. Vestibulum porttitor nunc enim, quis congue leo fringilla ut. Quisque in lacus lacus. Nunc malesuada nisi at porta convallis. In posuere interdum erat, et bibendum arcu. Etiam quis viverra diam.</p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className={Styles.right}>
+
+            </div>
           </div>
         </div>
       </div>
