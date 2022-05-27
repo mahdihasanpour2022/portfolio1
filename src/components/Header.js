94817c0cdb8b421b0fbd911da2758e0c6763e06e
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-05-27 13:27:41 -0700

    backdrop filter on header created

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,11 @@ const Header = () => {
       <div className="conyainer">
         <div className="row">
           <div className={Styles.headerContainer}>
-
+            <div className={Styles.headerContent}>
+              <h1>Elegant & Unique Design</h1>
+              <p>Right design and right ideas matter a lot of in interior design business.a style that makes a statement.</p>
+              <button>Read More</button>
+            </div>
           </div>
         </div>
       </div>
