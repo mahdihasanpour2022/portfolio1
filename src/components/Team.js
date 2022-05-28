f8a3b364c9f7aa419640a632950ff0e32057377b
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-05-28 08:39:54 -0700

    team style completed

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,15 +1,22 @@
 import React from 'react';
 // scss 
 import Styles from "../assets/style/components_style/Team.module.scss";
+import { comments_list } from "../data_service/coments_list";
 
 const Team = () => {
   return (
     <>
       <div className={`${Styles.TeamContainer} container-fluid`}>
-        <div className="row ">
-          <div className='col-10 offset-1'>
-            <div className={Styles.teamTitle}>
-              <p>Creative Team</p>
+        <div className="row">
+          <div className='col-12'>
+            <p>Creative Team</p>
+            <div className={Styles.teams}>
+              {comments_list.map(item =>
+                <div key={item.author} className={Styles.person}>
+                  <img src={item.image} alt={item.author} />
+                  <h4>{item.author}</h4>
+                  <p>{item.title}</p>
+                </div>)}
             </div>
           </div>
         </div>
--- src/data_service/coments_list.js
+++ src/data_service/coments_list.js
@@ -9,25 +9,25 @@ export const comments_list = [
   {
     id: 1,
     author: "jane smith",
-    title: "envato customer",
+    title: "General Manager",
     comment: " i just love thier design for all stunning details,you must know what can wew do for a project,before taking it,but with archo , the sky is a limit",
     image: autorImage1 ,
   }, {
     id: 2,
     author: "Mitchel Jhon",
-    title: "envato customer",
+    title: "Founder",
     comment: "i just love thier design for all stunning details,you must know what can wew do for a project,before taking it,but with archo , the sky is a limit",
     image:  autorImage2 ,
   }, {
     id: 3,
     author: "petter jeferson",
-    title: "envato customer",
+    title: "CEO Founder",
     comment: "i just love thier design for all stunning details,you must know what can wew do for a project,before taking it,but with archo , the sky is a limit",
     image:  autorImage3 ,
   }, {
     id: 4,
     author: "petter jeferson",
-    title: "envato customer",
+    title: "Designer",
     comment: "i just love thier design for all stunning details,you must know what can wew do for a project,before taking it,but with archo , the sky is a limit",
     image: autorImage4 
   }
