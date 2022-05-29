790db1138c85cbb5e818cc4a770bf055b09b8f62
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-05-29 01:28:25 -0700

    navbar component completed styled

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 import {useDispatch ,useSelector} from "react-redux";
 // action creator 
 import {closeSideMenu_AC} from "../redux/menus/sidemenu/sideMenuActionCreator";
 // scss 
 import Styles from "../assets/style/components_style/sidebar.module.scss";
 // icons 
-import { GrFormClose } from "react-icons/gr";
+import { IoIosClose } from "react-icons/io";
 
 const SideBar = () => {
 
@@ -18,7 +19,25 @@ const SideBar = () => {
     <>
       <div className={sidemenu ? `${Styles.sideMenuOpen} ${Styles.sideMenu}`: Styles.sideMenu}>
         <div>
-        <GrFormClose className={Styles.closeMenu} onClick={()=>dispatch(closeSideMenu_AC())}/>
+        <IoIosClose className={Styles.closeMenu} onClick={()=>dispatch(closeSideMenu_AC())}/>
+        </div>
+        <div className={Styles.menuItem}>
+        <h4>Archo</h4>
+        <hr className={Styles.horzLine} />
+        <ul className={Styles.navbarmenu}>
+          <li>
+            <NavLink to="/" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blogs" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>Blogs</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>Contact</NavLink>
+          </li>
+        </ul>
         </div>
       </div>
     </>
