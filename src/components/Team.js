f97ffcd729e1821e7a13cbd710c7dd99d4d6398b
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-05-29 10:40:57 -0700

    scroltop in team component created

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // scss 
 import Styles from "../assets/style/components_style/Team.module.scss";
+// data 
 import { comments_list } from "../data_service/coments_list";
+// icons 
+import { HiOutlineArrowCircleUp } from "react-icons/hi";
+
+
 
 const Team = () => {
+
+  const [downpage, setDownPage] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 3500) {
+        setDownPage(true)
+      }
+    })
+  }, [])
+
+  const goTop = () => {
+    window.scrollTo(0, 0);
+  }
+
+
   return (
     <>
       <div className={`${Styles.TeamContainer} container-fluid`}>
@@ -17,6 +38,12 @@ const Team = () => {
                   <h4>{item.author}</h4>
                   <p>{item.title}</p>
                 </div>)}
+            </div>
+            <div className={Styles.scrollspyIcon}>
+              <HiOutlineArrowCircleUp
+                onClick={() => goTop()}
+                style={downpage ? { opacity: 1 } : { opacity: 0 }}
+              />
             </div>
           </div>
         </div>
